d70d3f9e707dfb2b9144601b9220f442956b2156
AYH-TANG <yt2931@columbia.edu>
2024-12-12 01:22:30 -0500

    1212 0122

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://AYH-TANG.github.io",
+  site: "https://design-practices.github.io",
   base: "astro-maplibre-template",
   integrations: [mdx(), sitemap(), tailwind()],
 });
